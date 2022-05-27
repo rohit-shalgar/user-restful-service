ad67d2e533f64201c8b2c98b2d7ab04cb730213f
ROHIT RAJKUMAR SHALGAR(UST <208049@ust-global.com>
2022-05-27 17:40:20 -0500

    Open API, internationalization}

--- user-rest-service/src/main/java/com/rohit/learnings/userrestservice/internationalization/controller/HelloWorld.java
+++ user-rest-service/src/main/java/com/rohit/learnings/userrestservice/internationalization/controller/HelloWorld.java
@@ -3,6 +3,7 @@ package com.rohit.learnings.userrestservice.internationalization.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,13 @@ public class HelloWorld {
             @RequestHeader(name="Accept-Language",required = false)Locale locale){
 
         return messageSource.getMessage(HelloWorldMessages.GOOD_MORNING_MESSAGE,null,locale);
+
+    }
+
+    //you can pass the header, but it is not required to be explicitly used by the method.
+    @GetMapping(path = "/hello-world-without-header")
+    public String getHelloWordWithoutRequestHeader(){
+        return messageSource.getMessage(HelloWorldMessages.GOOD_MORNING_MESSAGE,null, LocaleContextHolder.getLocale());
 
     }
 
